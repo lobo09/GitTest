715b005c027684dacaffd4296b30da6ecfdd6569
Patrick Fischer <patrick.fischer@gmx.net>
2017-10-28 15:02:56 +0200

    Projektdateien hinzufügen.

---
+++ Caliburn.Micro Test2/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using Caliburn.Micro_Test2.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Caliburn.Micro_Test2
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
